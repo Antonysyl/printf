33b7352b43449f3bf2937e7c3d5a82c40da7a95e
Anthony Onyedika Chuka-Umeora <110993280+Antonysyl@users.noreply.github.com>
2022-10-15 10:04:08 +0100

    print numbers

---
+++ 1-printnumbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - int - handling conversioin specifiers %d, %i
+ * Description: creating the date which we started our alx journey till date
+ * Return: 0
+ */
+int main(void)
+{
+int started = 18, curr = 15;
+printf("We Started Our ALX Journey On The %dth of August\n", started);
+printf("We Are Currently Working On c-printf Project Today being %ith October\n", curr);
+return (0);
+ }
