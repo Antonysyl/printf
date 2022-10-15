623f544d16cc861c9e8800f5d9a8f1b0beee3920
Anthony Onyedika Chuka-Umeora <110993280+Antonysyl@users.noreply.github.com>
2022-10-15 22:34:30 +0100

    print numbers

--- 1-printnumbers.c
+++ 1-printnumbers.c
@@ -1,14 +1,42 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * main - int - handling conversioin specifiers %d, %i
- * Description: creating the date which we started our alx journey till date
- * Return: 0
+ * print_int - prints an integer
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
  */
-int main(void)
+int print_int(va_list arguments, char *buf, unsigned int ibuf)
 {
-int started = 18, curr = 15;
-printf("We Started Our ALX Journey On The %dth of August\n", started);
-printf("We Are Currently Working On c-printf Project Today being %ith October\n", curr);
-return (0);
- }
+	int int_input;
+	unsigned int int_in, int_temp, i, div, isneg;
+
+	int_input = va_arg(arguments, int);
+	isneg = 0;
+	if (int_input < 0)
+	{
+		int_in = int_input * -1;
+		ibuf = handl_buf(buf, '-', ibuf);
+		isneg = 1;
+	}
+	else
+	{
+		int_in = int_input;
+	}
+
+	int_temp = int_in;
+	div = 1;
+
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i + isneg);
+}
